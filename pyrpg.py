34a28af41a945ab8437d4d935d82f4d0eaaba106
MurkyMittens <murkymittens@mailmetrash.com>
2013-07-18 17:29:12 -0400

    made HP messages be a little more consistent

--- pyrpg.py
+++ pyrpg.py
@@ -7,6 +7,9 @@ def chanceRoll(probability):
 	chance_roll = randint(1, 100)
 	return chance_roll <= probability
 
+def sayHp(entity):
+	print "%s has %d/%d HP." % (entity.name, entity.getHealth(), entity.getMaximumHealth())
+
 def attack(attacker, defender):
 	if chanceRoll(75):
 		attack_value = attacker.getAttack()
@@ -15,7 +18,8 @@ def attack(attacker, defender):
 			if attack_value < 0:
 				attack_value = 0
 		defender.setHealth(defender.getHealth() - attack_value)
-		print "%s hits %s for %d. %s's health is now %d." % (attacker.name, defender.name, attack_value, defender.name, defender.getHealth())
+		print "%s hits %s for %d." % (attacker.name, defender.name, attack_value)
+		sayHp(defender)
 	else:
 		print "%s misses." % (attacker.name)
 
@@ -45,11 +49,13 @@ while player.isAlive():
 				player.setHealth(player.getHealth() * 0.25)
 				if player.getHealth() < 1:
 					player.setHealth(1)
-				print "You were careless and fell into a hole. Your HP is now %d." % (player.getHealth())
+				print "You were careless and fell into a hole."
+				sayHp(player)
 			else:
 				if player.getHealth() < player.getMaximumHealth():
 					player.setHealth(player.getHealth() + 1)
-					print "You've recovered 1 HP. Your HP is now %d." % (player.getHealth())
+					print "You've recovered 1 HP."
+					sayHp(player)
 				player.setExperience(player.getExperience() + 1)
 				print "You've received 1 EXP."
 		else:
@@ -67,17 +73,18 @@ while player.isAlive():
 				player.setHealth(player.getHealth() + 10)
 				player.setExperience(player.getExperience() + enemy.getExperience())
 				print "%s got %d EXP." % (player.name, enemy.getExperience())
-				print "%s's HP is now %d." % (player.name, player.getHealth())
 				if(player.getExperience() >= Player.EXPERIENCE_TARGET):
 					player.levelUp()
 					print "%s has leveled up!" % (player.name)
+				sayHp(player)
 		else:
 			print "You can't do that right now."
 	elif command == "health" or command == "h":
 		if player.getHealthPotions() > 0:
 			player.setHealth(player.getHealth() + 10)
 			player.setHealthPotions(player.getHealthPotions() - 1)
-			print "You've been healed for 10 HP. Your HP is now %d. You have %d health potions left." % (player.getHealth(), player.getHealthPotions())
+			print "You've been healed for 10 HP. You have %d health potions left." % (player.getHealthPotions())
+			sayHp(player)
 		else:
 			print "You don't have any more health potions."
 	elif command == "exit" or command == "x":
